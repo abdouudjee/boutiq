e4a9416b3da3c59d2815e62c3ce521a372832a28
abdouudjee <djedouanimohamedabdallah@gmail.com>
2025-05-22 20:06:59 +0000

    retrieving store logo,address,phone in the nav bar,retrieving the favicon in the layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
+	import { supabase } from '$lib';
 	import '../app.css';
+	import { onMount } from 'svelte';
+
 	let { children } = $props();
+	// retrieving url for favicon
+	onMount(async () => {
+		const favi = document.getElementById('favicon');
+		let { data: store_info, error } = await supabase.from('store_info').select('favicon_url');
+		favi.setAttribute('href', store_info[0].favicon_url);
+	});
 </script>
 
 {@render children()}
